48281cf09ecd8415653f29aa642ed4607dd0c9f9
naiik21 <nratia22@jaumebalmes.net>
2024-05-07 19:55:22 +0200

    Model de tot el projecte en client, ficar cada model en la seva aplicacio

--- botiga/clients/models.py
+++ botiga/clients/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.utils import timezone
+
+class Producte(models.Model):
+    nom = models.CharField(max_length=100)
+    descripcio = models.TextField()
+    unitats = models.IntegerField()
+    estaActiu = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Client(models.Model):
+    nom = models.CharField(max_length=100)
+    cognom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)  # Recuerda usar técnicas de almacenamiento seguro de contraseñas, como hash.
+
+class Carreto(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+class LlistaProductes(models.Model):
+    carreto = models.ForeignKey(Carreto, on_delete=models.CASCADE)
+    producte = models.ForeignKey(Producte, on_delete=models.CASCADE)
+    quantitat = models.IntegerField()
+
+class Ordre(models.Model):
+    carreto = models.OneToOneField(Carreto, on_delete=models.CASCADE)
+
+class Pagament(models.Model):
+    ordre = models.OneToOneField(Ordre, on_delete=models.CASCADE)
+    pagat = models.BooleanField(default=False)
