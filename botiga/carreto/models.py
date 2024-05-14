c7db5db9a6b5ffd12f5eddaeb89aef747345d1a7
naiik21 <nratia22@jaumebalmes.net>
2024-05-14 19:13:47 +0200

    Models carrito actualitzat

--- botiga/carreto/models.py
+++ botiga/carreto/models.py
@@ -5,5 +5,8 @@ from django.utils import timezone
 # Create your models here.
 class Carreto(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    estaActiu = models.BooleanField(default=True)
+    pagat = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(default=timezone.now)
     update_at = models.DateTimeField(auto_now=True)
