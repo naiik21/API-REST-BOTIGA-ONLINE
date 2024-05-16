16be691b23e52cad38a1a1872d1678e7271c70b0
naiik21 <nratia22@jaumebalmes.net>
2024-05-16 19:01:10 +0200

    carreto finalitzat

--- botiga/cataleg/serializer.py
+++ botiga/cataleg/serializer.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Producte
+from .models import LlistaProductes
 
 class ProducteSerializer(serializers.ModelSerializer):
     class Meta:
         model= Producte
         fields= '__all__'
+        
+class LlistaProductesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= LlistaProductes
+        fields= '__all__'
+
+
+class LlistaProductesProducteSerializer(serializers.ModelSerializer):
+    producte= ProducteSerializer()
+    class Meta:
+        model= LlistaProductes
+        fields= '__all__'
+        
